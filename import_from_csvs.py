fbae2f50f00e5def28a75f58e41e0f302432ff34
Joe Bylund <joseph.bylund@gmail.com>
2020-07-07 20:18:06 -0400

    (joe) I dunno, seems crazy

--- import_from_csvs.py
+++ import_from_csvs.py
@@ -1,6 +1,9 @@
+"""Import law enforcement agency information from CSVs in the data dir."""
+import argparse
 import csv
 import os
 import re
+import time
 
 import psycopg2
 import psycopg2.extras
@@ -10,29 +13,51 @@ from config import DBAUTH
 
 def import_one_state(cursor, filepath):
     with open(filepath) as fh:
+        print("Importing data for {}...".format(filepath))
         reader = csv.DictReader(fh)
-        # construct the insert statement...
-        for row in reader:
+        i = 0
+        for i, row in enumerate(reader, 1):
             cursor.execute(
                 """
-INSERT INTO depts_copy
+INSERT INTO departments
 (   name  ,   state  ,   dept_type  ,   url   ) VALUES
 ( %(name)s, %(state)s, %(dept_type)s, %(url)s ) ON CONFLICT DO NOTHING
                 """,
                 row,
             )
-        return
+        print("Imported {:,} departments...".format(i))
 
 
-def main():
-    """For each state ask wikipedia for a list of law enforcement agencies there and write those to DB."""
+def get_args():
+    parser = argparse.ArgumentParser(description=__doc__)
+    return vars(parser.parse_args())
+
+
+def ingest_from_csvs():
     with psycopg2.connect(**DBAUTH) as conn:
         conn.cursor_factory = psycopg2.extras.RealDictCursor
         cursor = conn.cursor()
-        for root, _, files in os.walk("data"):
+        cursor.execute("""DROP TABLE IF EXISTS departments""")
+        cursor.execute("""CREATE TABLE departments (name text, state text, dept_type text, url text)""")
+        cursor.execute("""CREATE UNIQUE INDEX ON departments (name, state)""")
+        cursor.execute("""CREATE UNIQUE INDEX ON departments (state, name)""")
+        for root, _, files in os.walk("data/departments/"):
             for ifile in files:
                 import_one_state(cursor, os.path.join(root, ifile))
         conn.commit()
+
+def main():
+    """For each state ask wikipedia for a list of law enforcement agencies there and write those to DB."""
+    get_args()
+    start = time.time()
+    while True:
+        try:
+            return ingest_from_csvs()
+        except Exception as oops:
+            if time.time() - start < 5:
+                time.sleep(0.5)
+            else:
+                raise
 
 
 if "__main__" == __name__:
--- makefile
+++ makefile
@@ -1,3 +1,24 @@
-all:
+LOG_PATH := /tmp/impact_police.log
+
+up: deploy ingest
+
+clean: down
+	/bin/rm -vf $(LOG_PATH)
+
+ingest: deploy
+	python import_from_csvs.py
+
+deploy: images
+	daemon --chdir=$(CURDIR) --output=$(LOG_PATH) -- docker-compose up
+
+lint:
+	python -m pip install --upgrade isort black > /dev/null
+	python -m black $(shell find . -name "*.py")
+	python -m isort $(shell find . -name "*.py")
+
+down:
+	docker-compose down
+
+images:
 	docker-compose build
-	docker-compose up
+	grep image docker-compose.yaml | cut -f 2- -d: | grep : | xargs -n1 -t docker pull
